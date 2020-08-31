a0b2e78af9ea5b8172c6ce321af68e3fd7ebbaac
Shreya Bhakta <shreyabhakta@hotmail.com>
2020-08-31 17:00:56 -0500

    Added PlayerMovement and CameraFollow Scripts

---
+++ 3D Graphics Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0f, 10f), ForceMode.Impulse);
+        }
+    }
+}
