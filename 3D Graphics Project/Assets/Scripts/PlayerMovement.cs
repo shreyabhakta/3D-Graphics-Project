a0e5717ddb794917ae8d59f6170a9ccdd9716911
avl170030 <50092984+avl170030@users.noreply.github.com>
2020-09-15 13:13:31 -0500

    Unity Collab Transfer

---
+++ 3D Graphics Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+    public float jumpSpeed = 7f;
+    public bool isLeftTurn = false;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            //FIXME: Play Animation while running
+            player.GetComponent<Animator>().Play("NormalAttack01_SwordShield");
+            Debug.Log("ATTACK");
+         
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            //Space Bar
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpSpeed), ForceMode.Impulse);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            //Right Arrow
+            Debug.Log("POSITIVE");
+            if (isLeftTurn)
+            {
+                transform.Rotate(0, 180, 0);
+                isLeftTurn = false;
+            }
+            player.GetComponent<Animator>().Play("Run_SwordShield");
+
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            //Left Arrow
+            Debug.Log("NEGATIVE");
+            if (isLeftTurn == false)
+            {
+                transform.Rotate(0, 180, 0);
+                isLeftTurn = true;
+            }
+            player.GetComponent<Animator>().Play("Run_SwordShield");
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
