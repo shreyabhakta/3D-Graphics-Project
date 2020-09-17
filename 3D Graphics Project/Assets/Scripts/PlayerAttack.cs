81a70e08ca01ad8e2c5d9b438979040d2a5ecf8d
kimjulia1117 <kimjulia1117@gmail.com>
2020-09-17 13:39:18 -0500

    Fixed jumping inconsistences, player dies when collided with enemy if not jumping on top of enemy, made physics material for player (but haven't attached it to the Polyart hero yet)

--- 3D Graphics Project/Assets/Scripts/PlayerAttack.cs
+++ 3D Graphics Project/Assets/Scripts/PlayerAttack.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+	public Transform spawnpoint;
+
 	void OnCollisionEnter(Collision other)
     {
 		if (other.transform.tag == "Enemy") {
 			if(other.contacts[0].normal.y >= 0.9f) {
-				print("Points colliding: " + other.contacts.Length);
-				print("First point that collided: " + other.contacts[0].normal);
 				Destroy(other.gameObject);
+			}
+			else {
+				Respawn();
 			}
 		}
     }
-}
+
+	void Respawn() {
+		this.transform.position = spawnpoint.position;
+	} 
+ }
--- 3D Graphics Project/Assets/Scripts/PlayerMovement.cs
+++ 3D Graphics Project/Assets/Scripts/PlayerMovement.cs
@@ -8,43 +8,26 @@ public class PlayerMovement : MonoBehaviour
     public float jumpSpeed = 7f;
     public bool isLeftTurn = false;
     public GameObject player;
-<<<<<<< HEAD
     public bool isGrounded = true;
-=======
->>>>>>> anh_main
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("1"))
-        {
+        { 
             //FIXME: Play Animation while running
-<<<<<<< HEAD
             player.GetComponent<Animator>().Play("NormalAttack01_SwordShield");         
         }
         else if (Input.GetButtonDown("Jump") && isGrounded)
         {
             //Space Bar
+            print("JUMP!");
             gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpSpeed), ForceMode.Impulse);
             isGrounded = false;        
-=======
-            player.GetComponent<Animator>().Play("NormalAttack01_SwordShield");
-            Debug.Log("ATTACK");
-         
-        }
-        else if (Input.GetButtonDown("Jump"))
-        {
-            //Space Bar
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpSpeed), ForceMode.Impulse);
->>>>>>> anh_main
         }
         else if (Input.GetAxis("Horizontal") > 0)
         {
             //Right Arrow
-<<<<<<< HEAD
-=======
-            Debug.Log("POSITIVE");
->>>>>>> anh_main
             if (isLeftTurn)
             {
                 transform.Rotate(0, 180, 0);
@@ -56,10 +39,6 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetAxis("Horizontal") < 0)
         {
             //Left Arrow
-<<<<<<< HEAD
-=======
-            Debug.Log("NEGATIVE");
->>>>>>> anh_main
             if (isLeftTurn == false)
             {
                 transform.Rotate(0, 180, 0);
@@ -71,15 +50,14 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
     }
-<<<<<<< HEAD
 
 	private void OnTriggerEnter(Collider other)
 	{
+        Debug.Log("other:" + other.transform.tag);
 		if (other.transform.tag == "Ground")
 		{
+            print("IS GROUNDED SHOULD BE TRUE!");
 			isGrounded = true;
 		}
 	}
-=======
->>>>>>> anh_main
 }
