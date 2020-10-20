d989e9640378ce3781f3504d1d25bcd30ba0a66d
shreya <shreyabhakta@hotmail.com>
2020-10-20 13:49:05 -0500

    testing master pull

---
+++ 3D Graphics Project/Assets/Scripts/Collisons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisons : MonoBehaviour
+{
+	public Transform spawnpoint;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Water")
+		{
+			Respawn();
+		}
+	}
+	
+	void Respawn() {
+		this.transform.position = spawnpoint.position;
+	}
+}
