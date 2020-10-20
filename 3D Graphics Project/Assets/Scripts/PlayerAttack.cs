ed0db6ad661c573b84d9d2d9188be7a0e94bc670
shreya <shreyabhakta@hotmail.com>
2020-10-20 14:32:42 -0500

    test

---
+++ 3D Graphics Project/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+	void OnCollisionEnter(Collision other)
+    {
+		if (other.transform.tag == "Enemy") {
+			if(other.contacts[0].normal.y >= 0.9f) {
+				print("Points colliding: " + other.contacts.Length);
+				print("First point that collided: " + other.contacts[0].normal);
+				Destroy(other.gameObject);
+			}
+		}
+    }
+}
