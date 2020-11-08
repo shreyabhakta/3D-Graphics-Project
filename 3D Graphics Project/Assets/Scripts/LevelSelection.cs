8a02ceab0a77daea1506449be64ad95f24b475fa
shreya <shreyabhakta@hotmail.com>
2020-11-08 16:15:47 -0600

    working menu and level selection

--- 3D Graphics Project/Assets/Scripts/LevelSelection.cs
+++ 3D Graphics Project/Assets/Scripts/LevelSelection.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class LevelSelection : MonoBehaviour {
 	public Button[] levelButtons;
-	public Image[] gates;
     // Start is called before the first frame update
     void Start() {
 		int levelAt = PlayerPrefs.GetInt("levelAt", 1);
@@ -13,10 +12,6 @@ public class LevelSelection : MonoBehaviour {
 		for(int i = 0; i < levelButtons.Length; i++) {
 			if( i + 1 > levelAt) {
 				levelButtons[i].interactable = false;
-				gates[i].enabled = true;
-			}
-			else {
-				gates[i].enabled = false;
 			}
 		}
     }
