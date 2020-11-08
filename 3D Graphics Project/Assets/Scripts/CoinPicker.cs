3171491c143039a1f3afe4c0cda4cf83447f2025
shreya <shreyabhakta@hotmail.com>
2020-11-08 17:05:48 -0600

    display coin count on canvas

--- 3D Graphics Project/Assets/Scripts/CoinPicker.cs
+++ 3D Graphics Project/Assets/Scripts/CoinPicker.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinPicker : MonoBehaviour
 {
+	public TextMeshProUGUI coinText;
 	public float coins = 0;
-	//public TextMeshProUGUI coinText;
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.transform.tag == "Coin")
 		{
 			coins++;
-			//coinText.text = coins.ToString();
+			coinText.text = coins.ToString();
 			Destroy(other.gameObject);
 		}
 	}
