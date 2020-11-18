5068548852871602aca5f00fcdfbb1debff880a4
shreya <shreyabhakta@hotmail.com>
2020-11-18 13:24:49 -0600

    add canvas and chest box collider + finish trigger on all levels

--- 3D Graphics Project/Assets/Scripts/PlayerAttack.cs
+++ 3D Graphics Project/Assets/Scripts/PlayerAttack.cs
@@ -1,9 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
+	public int health = 3;
+	public int numOfHearts = 3;
+	public RawImage[] hearts;
+	public RawImage fullHeart;
+	public RawImage emptyHeart;
+	public Transform spawnpoint;
+
+	// Update is called once per frame
+    void Update() 
+	{
+		if(health > numOfHearts) {
+			health = numOfHearts;
+		}
+        for(int i = 0; i < hearts.Length; i++) {
+        	if(i < health) {
+        		hearts[i].texture = fullHeart.texture;
+        	}
+			else {
+				hearts[i].texture = emptyHeart.texture;
+			}
+			
+			if(i < numOfHearts) {
+				hearts[i].enabled = true;
+			}
+			else {
+				hearts[i].enabled = false;
+			}
+        }
+    }
+
 	void OnCollisionEnter(Collision other)
     {
 		if (other.transform.tag == "Enemy") {
@@ -12,6 +43,19 @@ public class PlayerAttack : MonoBehaviour
 				print("First point that collided: " + other.contacts[0].normal);
 				Destroy(other.gameObject);
 			}
+			else {
+				--health;
+				Respawn();
+			}
+		}
+		if (other.transform.tag == "Water")
+		{
+			// --health;
+			Respawn();
 		}
     }
+	
+	void Respawn() {
+		this.transform.position = spawnpoint.position;
+	}
 }
